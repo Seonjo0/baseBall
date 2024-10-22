3bc7ddf2af750fb1567b54f9bd32cd23781c587a
Yipository <goatjswn.work@gmail.com>
2024-10-23 00:54:59 +0900

    Extract Method From raw Code

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     application
+
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 repositories {
@@ -18,10 +20,34 @@ java {
     }
 }
 
+sourceSets {
+    main {
+        java {
+            srcDir("src")
+        }
+    }
+}
+
 application {
     mainClass = "BaseballGame"
 }
 
 tasks.named<Test>("test") {
     useJUnitPlatform()
+}
+
+spotless {
+    java {
+        importOrder("java|javax", "", "com|org", "")
+
+        removeUnusedImports()
+
+        palantirJavaFormat("2.50.0")
+        formatAnnotations()
+    }
+
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint()
+    }
 }
--- src/BaseballGame.java
+++ src/BaseballGame.java
@@ -1,61 +1,79 @@
 import java.util.*;
 
+// 사랑해요 우리 산듀 사랑해요 결혼해용 내일 할까요? 강남에서 신고해용 <3
+import java.util.*;
+
 public class BaseballGame {
     public static void main(String[] args) {
-        System.out.println("실행됨");
-        Random random = new Random();
+        baseBallGame();
+    }
+
+    private static void baseBallGame() {
+        gameInit();
+    }
+
+    private static void gameInit() {
         Scanner scanner = new Scanner(System.in);
+        String[] correctAnswer = getAnswer();
+
+        System.out.println("==== 야구게임 시작! ====");
+        while (true) {
+            System.out.print("답을 입력하세요. q는 종료 : ");
+            String userInput = scanner.nextLine();
+
+            // 종료 및 정답 안내 분기점
+            if (isExtraInputs(userInput, correctAnswer)) break;
+            if (isCorrectInputs(userInput, correctAnswer)) break;
+        }
+    }
+
+    private static boolean isCorrectInputs(String answer, String[] correctAnswer) {
+        if (validateInputValues(answer)) {
+            String[] inputs = answer.split("");
+            HashMap<String, Integer> map = new LinkedHashMap<>(Map.of("Strike", 0, "Ball", 0, "Out", 0));
+
+            for (int i = 0; i < inputs.length; i++) {
+                String status = getString(correctAnswer, i, inputs);
+                map.put(status, map.getOrDefault(status, 0) + 1);
+            }
+
+            System.out.println("==결과==");
+            map.forEach((k, v) -> System.out.println(k + ": " + v));
+
+            if (map.getOrDefault("Strike", 0) == 3) {
+                System.out.println("Congratulations! You have won the game!");
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private static boolean isExtraInputs(String answer, String[] correctAnswer) {
+        if (answer.equals("q")) {
+            return true;
+        } else if (answer.equals("a")) {
+            System.out.print("정답은 ");
+            for(String a : correctAnswer){
+                System.out.print(a);
+            }
+            System.out.println("입니다. ");
+            return true;
+        }
+        return false;
+    }
+
+    private static String[] getAnswer() {
+        Random random = new Random();
         HashSet<String> strings = new LinkedHashSet<>();
 
-        while (true) {
+        do {
             int correct = random.nextInt(100, 999);
             strings.addAll((List.of(String.valueOf(correct).split(""))));
 
-            if (strings.size() == 3) {
-                break;
-            }
-        }
+        } while (strings.size() != 3);
 
-        String[] correctAnswer = strings.toArray(new String[0]);
-
-        System.out.println("==== 야구게임 시작! ====");
-
-        while (true) {
-            System.out.print("답을 입력하세요. q는 종료 : ");
-            String answer = scanner.nextLine();
-
-            // 종료 및 정답 안내 분기점
-            if (answer.equals("q")) {
-                break;
-            } else if (answer.equals("a")) {
-                System.out.print("정답은 ");
-                for(String a : correctAnswer){
-                    System.out.print(a);
-                }
-                System.out.println("입니다. ");
-                break;
-            }
-
-            if (validateInputValues(answer)) {
-                String[] inputs = answer.split("");
-                HashMap<String, Integer> map = new LinkedHashMap<>(Map.of("Strike", 0, "Ball", 0, "Out", 0));
-
-                for (int i = 0; i < inputs.length; i++) {
-                    String status = getString(correctAnswer, i, inputs);
-                    map.put(status, map.getOrDefault(status, 0) + 1);
-                }
-
-                System.out.println("==결과==");
-                map.forEach((k, v) -> System.out.println(k + ": " + v));
-
-                if (map.getOrDefault("Strike", 0) == 3) {
-                    System.out.println("Congratulations! You have won the game!");
-                    break;
-                }
-
-            }
-
-        }
+        return strings.toArray(new String[0]);
     }
 
     private static String getString(String[] stringCorrects, int i, String[] inputs) {
