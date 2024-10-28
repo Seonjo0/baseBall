bea004f949aef10f485554eddf89da21c1220c6e
Seonjo0 <129243915+Seonjo0@users.noreply.github.com>
2024-10-28 17:06:37 +0900

    change Enum packages

---
+++ src/enums/ResultStatus.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum ResultStatus {
+    CORRECT,
+    INCORRECT,
+    EXCEPTION
+}
---
+++ src/enums/UserInputStatus.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum UserInputStatus {
+    NULL,
+    QUIT,
+    HINT,
+    NORMAL
+}
